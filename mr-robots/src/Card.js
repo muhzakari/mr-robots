df5b9b0be938677f031e2c5b9a84fa293350b46a
Muhammad Zakari <mohdzakariyy@gmail.com>
2018-08-05 13:22:59 -0100

    first commit

---
+++ mr-robots/src/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Card = ({name,email,id}) => {
+ return (
+ 	
+	<div className='tc bg-light-blue dib br3 ma2 grow bw2 shadow-5' >
+	<img alt='robotz' src={`http://robohash.org/${id}?200x200`} />
+		
+		<div>
+			<h2>{name}</h2>
+			<p>{email}</p>
+		</div>
+
+	</div>
+
+ 	);
+ }
+
+export default Card;
---
+++ mr-robots/src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+    id: 1,
+    name: 'Emeka Jude',
+    username: 'Jud',
+    email: 'Emeka@naij.tv'
+  },
+  {
+    id: 2,
+    name: 'Idris Omar',
+    username: 'Omar',
+    email: 'Hamma@hodja.tv'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Chuks',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chimezie samuel',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dauda Ayo',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis David',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Rose V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Greene',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Karianne DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
